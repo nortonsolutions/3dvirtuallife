765556812191d36100c559ce94c3168c53b49877
David Norton <norton@whatever.com>
2021-12-04 21:39:38 -0700

    Working scene with fbx and cube.

--- views/viewScripts/scene.js
+++ views/viewScripts/scene.js
@@ -36,9 +36,12 @@ class Scene {
         scene.background = new THREE.Color( 'white' );
         scene.fog = new THREE.Fog( 'white', 0, 750 );
         
-        var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
+        var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, .75 );
         light.position.set( 0.5, 1, 0.75 );
         scene.add( light );
+
+        var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+        scene.add( directionalLight );
 
         this.controls = new THREE.PointerLockControls( camera );
 
@@ -113,72 +116,97 @@ class Scene {
         var floorGeometry = new THREE.PlaneBufferGeometry( this.planeWidth, this.planeHeight, this.widthSegments, this.heightSegments );
         floorGeometry.rotateX( - Math.PI / 2 );
     
-        // vertex displacement
+        // FLOOR VERTEX DISPLACEMENT
+        // var position = floorGeometry.attributes.position;  // Has a count
+        // for ( var i = 0, l = position.count; i < l; i ++ ) {
+        //     vertex.fromBufferAttribute( position, i );
+        //     vertex.x += Math.random() * 20 - 10;
+        //     vertex.y += Math.random() * 2;
+        //     vertex.z += Math.random() * 20 - 10;
+        //     position.setXYZ( i, vertex.x, vertex.y, vertex.z );
+        // }
+        // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
     
-        var position = floorGeometry.attributes.position;  // Has a count
-    
-        for ( var i = 0, l = position.count; i < l; i ++ ) {
-    
-            vertex.fromBufferAttribute( position, i );
-    
-            vertex.x += Math.random() * 20 - 10;
-            vertex.y += Math.random() * 2;
-            vertex.z += Math.random() * 20 - 10;
-    
-            position.setXYZ( i, vertex.x, vertex.y, vertex.z );
-    
-        }
-    
-        floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
-    
-        position = floorGeometry.attributes.position;
-        var colors = [];
-    
-        for ( var i = 0, l = position.count; i < l; i ++ ) {
-    
-            color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-            colors.push( color.r, color.g, color.b );
-    
-        }
-    
-        floorGeometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+        // FLOOR VERTEX COLORS
+        // position = floorGeometry.attributes.position;
+        // var colors = [];
+        // for ( var i = 0, l = position.count; i < l; i ++ ) {
+        //     color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+        //     colors.push( color.r, color.g, color.b );
+        // }
+        // floorGeometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
     
         var floorMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-    
         var floor = new THREE.Mesh( floorGeometry, floorMaterial );
         scene.add( floor );
     
-        // objects
-    
+        // BOXES
+        // var boxGeometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+        // boxGeometry = boxGeometry.toNonIndexed(); // ensure each face has unique vertices
+        // var position = boxGeometry.attributes.position;
+        // var colors = [];
+        // for ( var i = 0, l = position.count; i < l; i ++ ) {
+        //     color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+        //     colors.push( color.r, color.g, color.b );
+        // }
+        // boxGeometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+        // for ( var i = 0; i < 500; i ++ ) {
+        //     var boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: THREE.VertexColors } );
+        //     boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+        //     var box = new THREE.Mesh( boxGeometry, boxMaterial );
+        //     box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+        //     box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
+        //     box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+        //     scene.add( box );
+        //     this.objects.push( box );
+        // }
+
         var boxGeometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
-        boxGeometry = boxGeometry.toNonIndexed(); // ensure each face has unique vertices
-    
-        position = boxGeometry.attributes.position;
-        colors = [];
-    
+        boxGeometry = boxGeometry.toNonIndexed();
+        var position = boxGeometry.attributes.position;
+        var colors = [];
         for ( var i = 0, l = position.count; i < l; i ++ ) {
-    
             color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
             colors.push( color.r, color.g, color.b );
-    
         }
-    
+
         boxGeometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
-    
-        for ( var i = 0; i < 500; i ++ ) {
-    
-            var boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: THREE.VertexColors } );
-            boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-    
-            var box = new THREE.Mesh( boxGeometry, boxMaterial );
-            box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
-            box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
-            box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
-    
-            scene.add( box );
-            this.objects.push( box );
-    
-        }
+        
+        var boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: THREE.VertexColors } );
+        boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+        var box = new THREE.Mesh( boxGeometry, boxMaterial );
+        box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+        box.position.y = 10;
+        box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+        scene.add( box );
+        this.objects.push( box );
+        
+
+        // model
+        var loader = new THREE.FBXLoader();
+        loader.load( '/models/fbx/Sphere.fbx', function ( object ) {
+            // mixer = new THREE.AnimationMixer( object );
+            // var action = mixer.clipAction( object.animations[ 0 ] );
+            // action.play();
+
+            object.traverse( function ( child ) {
+
+                if ( child.isMesh ) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            } );
+
+            
+            scene.add( object );
+
+            object.position.x = 100;
+            object.position.z = 100;
+            object.scale.x = .5;
+            object.scale.y = .5;
+            object.scale.z = .5;
+            console.log("test");
+        } );
     
         renderer = new THREE.WebGLRenderer( { antialias: true } );
         renderer.setPixelRatio( window.devicePixelRatio );
@@ -218,7 +246,6 @@ class Scene {
         } );
 
         main.appendChild(renderer.domElement);
-
         window.addEventListener( 'resize', this.onWindowResize, false );
     }
 
@@ -250,7 +277,6 @@ class Scene {
 
             velocity.x -= velocity.x * 10.0 * delta;
             velocity.z -= velocity.z * 10.0 * delta;
-
             velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
 
             direction.z = Number( moveForward ) - Number( moveBackward );
@@ -271,24 +297,16 @@ class Scene {
             this.controls.getObject().translateY( velocity.y * delta );
             this.controls.getObject().translateZ( velocity.z * delta );
 
+            // Stay grounded
             if ( this.controls.getObject().position.y < 10 ) {
-
                 velocity.y = 0;
                 this.controls.getObject().position.y = 10;
-
                 canJump = true;
-
             }
-
             prevTime = time;
-
         }
-
         renderer.render( scene, camera );
-
     }
-
-
 }
 
 export {Scene};
